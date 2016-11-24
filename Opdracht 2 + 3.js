c3bc1569accfa087504c2d253e39e95eb65aa684
RobrechtS80 <r0383972@student.thomasmore.be>
2016-11-24 20:07:55 +0100

    Opdracht 3 volledig af (en ik denk ook correct)

--- Opdracht 2 + 3.js
+++ Opdracht 2 + 3.js
@@ -5,30 +5,44 @@ var accounts = [{username: "karen", balance: 10}];
 function deposit(account, amount)
 {
     if (typeof amount === 'number') {                            //https://developer.mozilla.org/nl/docs/Web/JavaScript/Reference/Operatoren/typeof
-    
-    account.balance = account.balance + amount;
-    }
-    
-    else { console.log("geef aub een geldig getal op voor amount.");}       //window.alert("geef aub een geldig getal op.")
+
+        account.balance = account.balance + amount;
+    } else {
+        console.log("geef aub een geldig getal op voor amount.");
+    }       //window.alert("geef aub een geldig getal op.")
 }
 
 function withdraw(account, amount)
 {
-    if (typeof amount ==='number'&&(account.balance-amount)>=0){                //mag geen leeg account hebben veronderstel ik .
-    account.balance = account.balance - amount;
+    if (typeof amount === 'number' && (account.balance - amount) >= 0) {                //mag geen leeg account hebben veronderstel ik .
+        account.balance = account.balance - amount;
+    } else {
+        console.log('geef aub een geldig getal op voor te storten');
     }
-    else{console.log('geef aub een geldig getal op voor te storten');}
 }
 
-function getBalance(account)
-{
 
+/*function makeAdder( basenumber ) {
+ return function ( numtoadd ) {
+ return numtoadd + basenumber;
+ }
+ }
+ */
+//anon func
+function getBalance()
+{
+    return function (account)
+    {
+        return account.balance;
+    };
+
+}
+
+//Standaard gwn
+
+function getBalance_std(account) {
     return account.balance;
 }
-
-//var balans= getBalance(); 
-//console.log(balans(lisa_acc));
-
 
 function createAccount(name, waarde)
 {
@@ -36,38 +50,40 @@ function createAccount(name, waarde)
     account.username = name;
     account.balance = waarde;
     accounts.push(account);
-return account;
+    return account;
 }
 
 //getaccount functie
-function getAccount (name) {
-	var matchedAccount;
+function getAccount(name) {
+    var matchedAccount;
 
-	for (var i = 0; i < accounts.length; i++) {
-		if (accounts[i].username === name) {
-			matchedAccount = accounts[i]; //matchedAccount = accounts[i].username  !!!! Waarom werkt dit niet ? als ik username mee geef weet die toch ook over welk account ik het heb, kan die zo geen positie bepalen ?
-                        //console.log(accounts[i].username);   //check if to see karen
-			break;
-		}
-	}
+    for (var i = 0; i < accounts.length; i++) {
+        if (accounts[i].username === name) {
+            matchedAccount = accounts[i]; //matchedAccount = accounts[i].username  !!!! Waarom werkt dit niet ? als ik username mee geef weet die toch ook over welk account ik het heb, kan die zo geen positie bepalen ?
+            //console.log(accounts[i].username);   //check if to see karen
+            break;
+        }
+    }
 
-	return matchedAccount;
+    return matchedAccount;
 }
+//var creatie voor func (like myfunc voorbeeld)
+var balans = getBalance();
 
 //account creator + check
 var lisa_acc = createAccount('lisa', 20);
 console.log(accounts);
 
-//deposit,withdraw check
+//deposit,withdraw check --> ook mogelijk natuurlijk console.log(balans(lisa_acc));
 deposit(lisa_acc, 60);
-console.log(getBalance(lisa_acc));
-withdraw(lisa_acc,15);
-console.log(getBalance(lisa_acc));
-
+console.log(getBalance_std(lisa_acc));
+withdraw(lisa_acc, 15);
+console.log(getBalance_std(lisa_acc));
+console.log(balans(lisa_acc));
 
 //getAccount check
 var existingAccount = getAccount("karen");
-console.log(getBalance(existingAccount));
+console.log(getBalance_std(existingAccount));
 
 //validation checker deposit
 //deposit(lisa_acc, 'az');
@@ -75,12 +91,11 @@ console.log(getBalance(existingAccount));
 
 //validation checker withdraw
 //withdraw(lisa_acc,'bf');
-withdraw(lisa_acc,66); // kom normaal -1 uit zien of hij zegt dat het niet klopt en gwn de vorige balance teruggeven.
-console.log(getBalance(lisa_acc));
+withdraw(lisa_acc, 66); // kom normaal -1 uit zien of hij zegt dat het niet klopt en gwn de vorige balance teruggeven.
+console.log(getBalance_std(lisa_acc));
 
-
-
+console.log(balans(lisa_acc));
 /*Revisit your bank account practice
- Add a function that returns a anonymous function that returns the
-balance from the given account
-*/
+  Add a function that returns a anonymous function that returns the
+ balance from the given account
+ */
